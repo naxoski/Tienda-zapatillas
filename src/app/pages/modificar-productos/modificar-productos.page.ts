7dac745af7c1e17704e2dc4d1f8dd8866a9be085
Bstengerr <benj.urra@duocuc.cl>
2023-10-15 14:44:18 -0300

    modificar usuario y modificar producto(falta revisar)

--- src/app/pages/modificar-productos/modificar-productos.page.ts
+++ src/app/pages/modificar-productos/modificar-productos.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modificar-productos',
-  templateUrl: './modificar-productos.page.html',
-  styleUrls: ['./modificar-productos.page.scss'],
+  templateUrl: 'modificar-productos.page.html',
+  styleUrls: ['modificar-productos.page.scss'],
 })
-export class ModificarProductosPage implements OnInit {
+export class ModificarProductosPage {
+  nombreProducto: string = '';
+  nuevoPrecio: number = 0;
+  nuevoStock: number = 0;
 
-  constructor() { }
+  productos: any[] = [
+    { nombre: 'Zapatillas 1', precio: 50, stock: 10 },
+    { nombre: 'Zapatillas 2', precio: 60, stock: 5 },
+    { nombre: 'Zapatillas 3', precio: 70, stock: 8 },
+  ];
 
-  ngOnInit() {
+  modificarProducto() {
+    // Validar que los campos no estén vacíos y que el precio y el stock sean números válidos.
+    if (this.nombreProducto.trim() === '' || isNaN(this.nuevoPrecio) || isNaN(this.nuevoStock)) {
+      console.log("Por favor, complete todos los campos con valores válidos.");
+      return;
+    }
+
+    for (let producto of this.productos) {
+      if (producto.nombre === this.nombreProducto) {
+        producto.precio = this.nuevoPrecio;
+        producto.stock = this.nuevoStock;
+        console.log(`Producto '${this.nombreProducto}' modificado con éxito.`);
+        break;
+      }
+    }
   }
-
 }
+
--- src/app/pages/modificar-usuario/modificar-usuario.page.ts
+++ src/app/pages/modificar-usuario/modificar-usuario.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modificar-usuario',
-  templateUrl: './modificar-usuario.page.html',
-  styleUrls: ['./modificar-usuario.page.scss'],
+  templateUrl: 'modificar-usuario.page.html',
+  styleUrls: ['modificar-usuario.page.scss'],
 })
-export class ModificarUsuarioPage implements OnInit {
+export class ModificarUsuarioPage {
+  nombreUsuario: string = '';
+  nuevoNombre: string = '';
+  nuevoEmail: string = '';
 
-  constructor() { }
+  usuarios: any[] = [
+    { nombreUsuario: 'usuario1', nombre: 'Nombre1', email: 'email1@example.com' },
+    { nombreUsuario: 'usuario2', nombre: 'Nombre2', email: 'email2@example.com' },
+    { nombreUsuario: 'usuario3', nombre: 'Nombre3', email: 'email3@example.com' },
+  ];
 
-  ngOnInit() {
+  modificarUsuario() {
+    for (let usuario of this.usuarios) {
+      if (usuario.nombreUsuario === this.nombreUsuario) {
+        usuario.nombre = this.nuevoNombre;
+        usuario.email = this.nuevoEmail;
+        
+        console.log(`Usuario '${this.nombreUsuario}' modificado con éxito.`);
+        break;
+      }
+    }
   }
-
 }
