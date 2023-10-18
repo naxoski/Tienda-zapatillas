41b19707b0113d4c3d10ff8b7432f090f1ac6a0a
Bstengerr <benj.urra@duocuc.cl>
2023-10-18 19:47:43 -0300

    dsp lo borro

--- src/app/pages/crear-cuenta/crear-cuenta.page.ts
+++ src/app/pages/crear-cuenta/crear-cuenta.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
 import { DbservicesService } from 'src/app/services/dbservices.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-crear-cuenta',
@@ -33,6 +34,7 @@ export class CrearCuentaPage implements OnInit {
   Validador6 = false;
   Validador7 = false;
   Validador8 = false;
+  imageSource: any ;
 
 
  
@@ -186,6 +188,24 @@ export class CrearCuentaPage implements OnInit {
   }
   ngOnInit() {
   }
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90, //Este es la calidad, el 90 significa el 90% de calidad
+      allowEditing: true,
+      //Este es para que edite directamente
+      resultType: CameraResultType.DataUrl, //El como quiero guardarla, como quiero configurarla. Lo mejor que haremos con la url es que la guardemos como data, para que cuando la veamos en la pagina salga en la url
+      source: CameraSource.Prompt  //Esto es para pdamos elegir el tipo de camera 
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    //var imageUrl = image.webPath;
+    this.imageSource = image.dataUrl;
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+  };
 
 }
 
