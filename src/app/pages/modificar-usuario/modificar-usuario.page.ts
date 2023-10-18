d80afdc991885e967f036c54b01c1763426b803b
Bstengerr <benj.urra@duocuc.cl>
2023-10-18 19:32:22 -0300

    a

--- src/app/pages/modificar-usuario/modificar-usuario.page.ts
+++ src/app/pages/modificar-usuario/modificar-usuario.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-modificar-usuario',
@@ -11,10 +12,10 @@ export class ModificarUsuarioPage {
   nuevoEmail: string = '';
 
   usuarios: any[] = [
-    { nombreUsuario: 'usuario1', nombre: 'Nombre1', email: 'email1@example.com' },
-    { nombreUsuario: 'usuario2', nombre: 'Nombre2', email: 'email2@example.com' },
-    { nombreUsuario: 'usuario3', nombre: 'Nombre3', email: 'email3@example.com' },
-  ];
+
+];
+
+  imageSource: any ;
 
   modificarUsuario() {
     for (let usuario of this.usuarios) {
@@ -25,6 +26,25 @@ export class ModificarUsuarioPage {
         console.log(`Usuario '${this.nombreUsuario}' modificado con éxito.`);
         break;
       }
+    
     }
   }
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90, //Este es la calidad, el 90 significa el 90% de calidad
+      allowEditing: true,
+      //Este es para que edite directamente
+      resultType: CameraResultType.DataUrl, //El como quiero guardarla, como quiero configurarla. Lo mejor que haremos con la url es que la guardemos como data, para que cuando la veamos en la pagina salga en la url
+      source: CameraSource.Prompt  //Esto es para pdamos elegir el tipo de camera 
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    //var imageUrl = image.webPath;
+    this.imageSource = image.dataUrl;
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+  };
 }
