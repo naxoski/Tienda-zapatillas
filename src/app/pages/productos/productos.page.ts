aa7e0854b2abdb51aee80f150a4ad5f3ba0c5a9e
ignacio <i.huerta@duocuc.cl>
2023-10-19 18:25:27 -0300

    carrito funcional al agregar productos
    aaaaa

--- src/app/pages/productos/productos.page.ts
+++ src/app/pages/productos/productos.page.ts
@@ -20,7 +20,7 @@ export class ProductosPage implements OnInit {
   usuario: any = {idusuario: '', rut: '', nombreusuario: '', apellidousuario: '',fnacimiento: '', telefono: '', fotoperfil: '' ,correo: '',clave: '', respuesta: '',idpregunta:'',idrol:''};
   venta: any =[{idventa:'',fventa:'',fdespacho:'',estatus:'',total:'',carrito:'',idusuario:''}];
   fechaActual = new Date();
-  diasSumar = 20;
+  diasSumar = 7;
   fdespacho = new Date(this.fechaActual);
   detalle: any = [{iddetalle: '', cantidad: '',detalle:'',idproducto:'',idventa:''}];
   detalles:any =[{iddetalle:'',cantidad:'',detalle:'',idproducto:'',idventa:'',nombreproducto:'', precio:'',stock:'',foto:''}];
@@ -56,6 +56,8 @@ export class ProductosPage implements OnInit {
             this.detalle = detalles[0];
             console.log("ID DEL DETALLE ENCONTRADO: " + this.detalle.iddetalle);
             this.db.modificarDetalle(this.detalle.iddetalle, this.detalle.detalle + precio, this.detalle.cantidad + 1);
+            console.log("Cantidad:"+this.detalle.cantidad);
+            console.log("Cantidad:"+this.detalle.cantidad+1);
             this.db.modificarTotal(this.venta.idventa, this.venta.total + precio);
             console.log("-------------------------------------");
             console.log("  Se está modificando el detalle ya previamente existente");
@@ -77,7 +79,7 @@ export class ProductosPage implements OnInit {
         await this.db.insertarVenta(this.fechaActual,this.fdespacho, 'Activo', precio, 'C', this.idUser);
 
         this.db.fetchVenta().subscribe(venta2 => {
-          this.venta = venta2[venta2.length - 1];
+          this.venta = venta2[venta2.length + 1];
           console.log("ID de la venta que se generó: "+this.venta.idventa);
           this.db.agregarDetalle(1, precio, this.venta.idventa, idprod);
 
