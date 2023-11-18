9ec6addf499e9ad2b28b9791445fb83056d584ed
Bstengerr <benj.urra@duocuc.cl>
2023-11-18 16:25:56 -0300

    a

--- src/app/pages/cambiarcontra/cambiarcontra.page.ts
+++ src/app/pages/cambiarcontra/cambiarcontra.page.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cambiarcontra.page.scss'],
 })
 export class CambiarcontraPage implements OnInit {
+  clave1= "";
+  clave2= "";
 
   constructor() { }
 
