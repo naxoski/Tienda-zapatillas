0b19e4fde36cb765336c88557f85f99f02bef366
ignacio <i.huerta@duocuc.cl>
2023-11-19 22:35:28 -0300

    weas
    aaaaa

--- src/app/pages/verusuarios/verusuarios.page.spec.ts
+++ src/app/pages/verusuarios/verusuarios.page.spec.ts
@@ -20,6 +20,40 @@ describe('VerusuariosPage', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VerusuariosPage);
     component = fixture.componentInstance;
+
+    // Modificar el componente para asignar un array de usuarios a la propiedad 'usuarios'
+    component.usuario = [
+      {
+        idusuario: 500,
+        rut: '21.475.570-k',
+        nombreusuario: 'ignacio',
+        apellidousuario: 'huerta',
+        fnacimiento: '2004-01-05',
+        telefono: 123456789,
+        fotoperfil: 'assets/chad.webp',
+        correo: 'ignaciohuerta8a@gmail.com',
+        clave: 'claveprueba123',
+        respuesta: 'si',
+        idpregunta: '¿Tienes pareja?',
+        idrol: 'usuario'
+      },
+      {
+        idusuario: 600,
+        rut: '21.475.571-k',
+        nombreusuario: 'ignacio',
+        apellidousuario: 'huerta',
+        fnacimiento: '2004-01-05',
+        telefono: 123456789,
+        fotoperfil: 'assets/chad.webp',
+        correo: 'administrador@gmail.com',
+        clave: 'claveprueba123',
+        respuesta: 'si',
+        idpregunta: '¿Tienes pareja?',
+        idrol: 'admin'
+      },
+      // Puedes agregar más usuarios según sea necesario
+    ];
+
     fixture.detectChanges();
   });
 
@@ -27,3 +61,4 @@ describe('VerusuariosPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
