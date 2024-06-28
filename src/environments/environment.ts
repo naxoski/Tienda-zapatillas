7ae6d9c515cc929370fddd0f04d9ae0b481bbe23
naxoski <i.huerta@duocuc.cl>
2024-06-27 22:09:44 -0400

    a
    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { initializeApp } from "firebase/app";
 
 export const environment = {
   production: false,
@@ -17,17 +16,7 @@ export const environment = {
     
   },
 };
-const firebaseConfig = {
-  apiKey: "AIzaSyAH-x2KoE0E6SvHQuaiH3CprH0n4VKNLDY",
-  authDomain: "tienda-zapatillas-b496f.firebaseapp.com",
-  databaseURL: "https://tienda-zapatillas-b496f-default-rtdb.firebaseio.com",
-  projectId: "tienda-zapatillas-b496f",
-  storageBucket: "tienda-zapatillas-b496f.appspot.com",
-  messagingSenderId: "111178115209",
-  appId: "1:111178115209:web:9ea59e8ccb9bc65154be58"
-};
 
-const app = initializeApp(firebaseConfig);
 
 
 
